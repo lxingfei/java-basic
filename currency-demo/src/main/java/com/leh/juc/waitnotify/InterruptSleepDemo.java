ae4b49ac80215b8f4b304f80953f657cd73a8e8b
lveh <lveh@tsfa.com.cn>
2019-09-18 11:24:34 +0800

    interrupt

--- currency-demo/src/main/java/com/leh/juc/waitnotify/InterruptSleepDemo.java
+++ currency-demo/src/main/java/com/leh/juc/waitnotify/InterruptSleepDemo.java
@@ -9,27 +9,30 @@ package com.leh.juc.waitnotify;
  * the precision and accuracy of system timers and schedulers. The thread
  * does not lose ownership of any monitors.
  */
-public class SleepDemo {
+public class InterruptSleepDemo {
     public static void main(String[] args) throws InterruptedException {
         Object lock = new Object();
 
-        new Thread(() -> {
+        Thread t1 = new Thread(() -> {
             System.out.println("111111111111");
             synchronized (lock) {
                 System.out.println("222222222222");
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(5000);
                     System.out.println("333333333333");
                 } catch (InterruptedException e) {
-                    System.out.println("xxxxxxxxxxxx when interrupt");
+                    System.out.println("xxxxxxxxxxxx when interrupt t1");
                 }
 
             }
 
-        }, "t1").start();
+        }, "t1");
 
-        // 主线程睡眠 10ms 保证 t1 和 t2 的先后顺序
+        t1.start();
+
+        // 主线程睡眠 10ms 模拟顺序执行
         Thread.sleep(10);
+
 
         new Thread(() ->{
             System.out.println("444444444444");
@@ -41,16 +44,23 @@ public class SleepDemo {
             }
 
         }, "t2").start();
+
+        /*
+            t1被打断，释放锁，捕获InterruptedException异常，t2获得锁
+
+         */
+        t1.interrupt();
+
+
     }
 
     /*
         output：
         111111111111
         222222222222
+        xxxxxxxxxxxx when interrupt t1
         444444444444
-
-        ----2s后----
-        333333333333
         555555555555
+
      */
 }
